795c4579fbc9113b03359ddbc2a5896931864fdd
Алексей Н. Вовк <van@ets.nnov.ru>
2023-08-01 19:45:53 +0300

    Программа сложения  2 чисел с функцией рандомизации

--- Examle003_Sum/Program.cs
+++ Examle003_Sum/Program.cs
@@ -1,3 +1,6 @@
-﻿int iNumberA = 3;
-int iNumberB = 5;
-Console.WriteLine(iNumberA + iNumberB);
+﻿int iNumberA = new Random().Next(1, 10);
+Console.WriteLine(iNumberA);
+int iNumberB = new Random().Next(1, 10);
+Console.WriteLine(iNumberB);
+int iResult = iNumberA + iNumberB;
+Console.WriteLine(iResult);
